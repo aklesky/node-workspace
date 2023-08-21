cba13d3e61955090df4f99308bd070053fd828a0
Aleksey Semiglasov <aleksey.semiglasov@gmail.com>
2023-08-21 10:32:19 +0200

    fix(types): Fixing Types annotations
    update type annotation in tailiwindcss/plugin

--- packages/tailwind-config/src/plugin.ts
+++ packages/tailwind-config/src/plugin.ts
@@ -1,7 +1,8 @@
 import tailwind, { Config } from 'tailwindcss'
+import { Plugin, Processor } from 'postcss'
 import getTailwindConfig from './config'
 import { TailwindOptions } from './interfaces'
 
 export = (config?: Config, options?: TailwindOptions) => {
-    return tailwind(getTailwindConfig(config, options))
+    return tailwind(getTailwindConfig(config, options)) as Plugin | Processor
 }
