79848ae1169690115f5a8f8d2dd19d293ba1c817
Aleksey Semiglasov <aleksey.semiglasov@gmail.com>
2023-12-27 14:57:31 +0100

    feat(vite): update configuration
    Update the Vite configuration to enhance build performance and compatibility.

--- packages/vite-config/src/presets/base.ts
+++ packages/vite-config/src/presets/base.ts
@@ -5,6 +5,18 @@ const root = process.cwd()
 export const base = defineConfig({
     root: `${root}/src`,
     publicDir: `${root}/public`,
+    optimizeDeps: {
+        exclude: [`${root}/src/__tests__`, `${root}/src/__fixtures__`, `${root}/src/__mocks__`],
+    },
+    build: {
+        outDir: `${root}/dist`,
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                dir: `${root}/dist`,
+            },
+        },
+    },
     server: {
         port: (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000,
     },
